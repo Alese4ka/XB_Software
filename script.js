3275c3c95c025b094af2a389e5960bd59fbccba6
Alesya Branavitskaya <alesun4ik@gmail.com>
2022-02-16 15:58:23 +0300

    develop: -create function getSumOfCurrency

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //нельзя вводить отрицат
 
+
 function start(){
   let numberOfCurrency = document.getElementById("number").value;
   let selectedCurrency = document.getElementById("currency").value;
@@ -8,96 +9,76 @@ function start(){
   let numberOfThird = document.querySelector(".third-currency");
   let numberOfFourth = document.querySelector(".fourth-currency");
   let sum;
+
+  function getSumOfCurrency(value) {
+    sum = (numberOfCurrency*value).toFixed(2);
+  }  
+
   switch(selectedCurrency) {
     case 'BYN':  
-      //2.5846 USD
-      sum = numberOfCurrency/2.5846;
-      sum = sum.toFixed(2)+ " USD";
-      numberOfFirst.innerHTML = `${sum}`; 
-      //2.9306 EUR
-      sum = numberOfCurrency/2.9306;
-      sum = sum.toFixed(2)+ " EUR";
-      numberOfSecond.innerHTML = `${sum}`; 
-      //0.41 CNY
-      sum = numberOfCurrency/0.41;
-      sum = sum.toFixed(2)+ " CNY";
-      numberOfThird.innerHTML = `${sum}`; 
-      //3.4052 RUB
-      sum = numberOfCurrency/3.4052*100;
-      sum = sum.toFixed(2)+ " RUB";
-      numberOfFourth.innerHTML = `${sum}`; 
+      getSumOfCurrency(0.39);
+      numberOfFirst.innerHTML = `${sum + " USD"}`; 
+
+      getSumOfCurrency(0.34);
+      numberOfSecond.innerHTML = `${sum + " EUR"}`; 
+      
+      getSumOfCurrency(2.46);
+      numberOfThird.innerHTML = `${sum + " CNY"}`; 
+      
+      getSumOfCurrency(29.30);
+      numberOfFourth.innerHTML = `${sum + " RUB"}`; 
       break;
     case 'USD':   
-      //BYN
-      sum = numberOfCurrency*2.5846;
-      sum = sum.toFixed(2)+ " BYN";
-      numberOfFirst.innerHTML = `${sum}`; 
-      //EUR
-      sum = numberOfCurrency*0.88;
-      sum = sum.toFixed(2)+ " EUR";
-      numberOfSecond.innerHTML = `${sum}`; 
-      //CNY
-      sum = numberOfCurrency*6.31;
-      sum = sum.toFixed(2)+ " CNY";
-      numberOfThird.innerHTML = `${sum}`; 
-      //RUB
-      sum = numberOfCurrency*75.10;
-      sum = sum.toFixed(2)+ " RUB";
-      numberOfFourth.innerHTML = `${sum}`; 
+      getSumOfCurrency(2.5846);
+      numberOfFirst.innerHTML = `${sum + " BYN"}`; 
+      
+      getSumOfCurrency(0.88);
+      numberOfSecond.innerHTML = `${sum + " EUR"}`; 
+      
+      getSumOfCurrency(6.31);
+      numberOfThird.innerHTML = `${sum + " CNY"}`; 
+
+      getSumOfCurrency(75.10);
+      numberOfFourth.innerHTML = `${sum + " RUB"}`; 
       break;
     case 'EUR':    
-      //BYN
-      sum = numberOfCurrency*2.92;
-      sum = sum.toFixed(2)+ " BYN";
-      numberOfFirst.innerHTML = `${sum}`; 
-      //USD
-      sum = numberOfCurrency*1.14;
-      sum = sum.toFixed(2)+ " USD";
-      numberOfSecond.innerHTML = `${sum}`; 
-      //CNY
-      sum = numberOfCurrency*7.18;
-      sum = sum.toFixed(2)+ " CNY";
-      numberOfThird.innerHTML = `${sum}`; 
-      //RUB
-      sum = numberOfCurrency*85.47;
-      sum = sum.toFixed(2)+ " RUB";
-      numberOfFourth.innerHTML = `${sum}`; 
+      getSumOfCurrency(2.92);
+      numberOfFirst.innerHTML = `${sum + " BYN"}`; 
+      
+      getSumOfCurrency(1.14);
+      numberOfSecond.innerHTML = `${sum + " USD"}`; 
+      
+      getSumOfCurrency(7.18);
+      numberOfThird.innerHTML = `${sum + " CNY"}`; 
+      
+      getSumOfCurrency(85.47);
+      numberOfFourth.innerHTML = `${sum + " RUB"}`; 
       break;
     case 'CNY':  
-      //BYN
-      sum = numberOfCurrency*0.41;
-      sum = sum.toFixed(2)+ " BYN";
-      numberOfFirst.innerHTML = `${sum}`; 
-      //USD
-      sum = numberOfCurrency*1.16;
-      sum = sum.toFixed(2)+ " USD";
-      numberOfSecond.innerHTML = `${sum}`; 
-      //EUR
-      sum = numberOfCurrency*0.14;
-      sum = sum.toFixed(2)+ " EUR";
-      numberOfThird.innerHTML = `${sum}`; 
-      //RUB
-      sum = numberOfCurrency*11.90;
-      sum = sum.toFixed(2)+ " RUB";
-      numberOfFourth.innerHTML = `${sum}`; 
+      getSumOfCurrency(0.41);
+      numberOfFirst.innerHTML = `${sum + " BYN"}`; 
+      
+      getSumOfCurrency(1.16);
+      numberOfSecond.innerHTML = `${sum + " USD"}`; 
+      
+      getSumOfCurrency(0.14);
+      numberOfThird.innerHTML = `${sum + " EUR"}`; 
+      
+      getSumOfCurrency(11.90);
+      numberOfFourth.innerHTML = `${sum + " RUB"}`; 
       break;
     case 'RUB':  
-      //BYN
-      sum = numberOfCurrency*3.41/100;
-      sum = sum.toFixed(2)+ " BYN";
-      numberOfFirst.innerHTML = `${sum}`; 
-      //USD
-      sum = numberOfCurrency*1.33/100;
-      sum = sum.toFixed(2)+ " USD";
-      numberOfSecond.innerHTML = `${sum}`; 
-      //CNY
-      sum = numberOfCurrency*1.17/100;
-      sum = sum.toFixed(2)+ " EUR";
-      numberOfThird.innerHTML = `${sum}`; 
-      //CNY
-      sum = numberOfCurrency*8.41/100;
-      sum = sum.toFixed(2)+ " CNY";
-      numberOfFourth.innerHTML = `${sum}`; 
+      getSumOfCurrency(0.0341);
+      numberOfFirst.innerHTML = `${sum + " BYN"}`; 
+      
+      getSumOfCurrency(0.0133);
+      numberOfSecond.innerHTML = `${sum + " USD"}`;
+
+      getSumOfCurrency(0.0117);
+      numberOfThird.innerHTML = `${sum + " EUR"}`; 
+
+      getSumOfCurrency(0.0841);
+      numberOfFourth.innerHTML = `${sum + " CNY"}`; 
       break;
     default:
       break;
